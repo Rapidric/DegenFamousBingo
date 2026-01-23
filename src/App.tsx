ed85d2f76406cdd0c7c50b3bc40f35aab0fed43b
[dyad] <git@dyad.sh>
2026-01-23 10:37:32 -0800

    [dyad] Removing unused toast system - wrote 1 file(s), deleted 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "sonner";
+import { Toaster as Sonner } from "sonner"; // Sonner is used for toasts
 import { TooltipProvider } from "@/components/ui/tooltip";
 
 // Import components
@@ -24,7 +23,7 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
+      {/* Only Sonner Toaster is used */}
       <Sonner />
       <BrowserRouter>
         <div className="flex flex-col min-h-screen">
